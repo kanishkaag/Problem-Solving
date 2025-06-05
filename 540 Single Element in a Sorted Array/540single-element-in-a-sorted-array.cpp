ef12ff7eed4605d57a3681415688a91faf7ddb04
kanishkaag <kanishkaagrawal28@gmail.com>
2025-06-05 07:58:40 +0000

    Code challenge submission for question: 540 Single Element in a Sorted Array

---
+++ 540 Single Element in a Sorted Array/540single-element-in-a-sorted-array.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        int st = 0;
+        int end = n - 1;
+        if( n == 1) return nums[0];
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+            // Edge cases
+            if (mid == 0 && nums[0] != nums[1])
+                return nums[mid];
+            if (mid == n - 1 && nums[n - 1] != nums[n - 2])
+                return nums[mid];
+
+            if (nums[mid - 1] != nums[mid] && nums[mid] != nums[mid + 1]) {
+                return nums[mid];
+            }
+
+            if (mid % 2 == 0) { // even
+                if (nums[mid - 1] == nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    st = mid + 1;
+                }
+            } else {                              // odd
+                if (nums[mid - 1] == nums[mid]) { // right
+                    st = mid + 1;
+                } else { // left
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
