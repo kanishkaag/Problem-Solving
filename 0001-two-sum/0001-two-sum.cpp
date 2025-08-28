b9d41a552a318d37385d64f6b6d7ce64dde35d25
Kanishka Agrawal <112953546+kanishkaag@users.noreply.github.com>
2025-08-28 19:02:26 +0530

    Time: 1 ms (80.09%), Space: 14.8 MB (36.14%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int sum;
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                sum = nums[i] + nums[j];
-                if (sum == target) {
-                    return {i, j}; // Return the indices
-                }
+        unordered_map<int,int> mpp;
+        int complement = 0;
+        for(int i = 0;i<nums.size();i++){
+            complement = target - nums[i];
+            if(mpp.find(complement)!=mpp.end()){
+                return {mpp[complement], i};
             }
+            mpp[nums[i]] = i;
         }
-        return {}; // Return an empty vector if no solution is found (for safety)
+
+
+
+        // int sum;
+        // for (int i = 0; i < nums.size(); i++) {
+        //     for (int j = i + 1; j < nums.size(); j++) {
+        //         sum = nums[i] + nums[j];
+        //         if (sum == target) {
+        //             return {i, j}; // Return the indices
+        //         }
+        //     }
+        // }
+        // return {}; // Return an empty vector if no solution is found (for safety)
+        return {};
     }
 };
