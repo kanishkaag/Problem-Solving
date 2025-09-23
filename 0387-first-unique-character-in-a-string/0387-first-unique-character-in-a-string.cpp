d1d15d07439367c8f0c326271457217e0379db37
Kanishka Agrawal <112953546+kanishkaag@users.noreply.github.com>
2025-09-23 23:39:19 +0530

    Time: 29 ms (10.03%), Space: 14.9 MB (23.14%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,17 +1,40 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
+        // //Brute Force
+        // for(int i = 0;i<s.size();i++){
+        //     int count = 0;
+        //     for(int j = 0;j<s.size();j++){
+        //         if(i != j && s[i] == s[j]){
+        //             count = 1;
+        //             break;
+        //         }
+        //     }
+        //     if(count == 0) return i;
+        //     }
+        // return -1;   
+
+        //Optimized
+        
+        unordered_map<char,int>mpp;
+        queue<int> q; //in queue i will store the index of char as i have 
+                      //to   return that in ans
+
         for(int i = 0;i<s.size();i++){
-            int count = 0;
-            for(int j = 0;j<s.size();j++){
-                if(i != j && s[i] == s[j]){
-                    count = 1;
-                    break;
-                }
+            //If a character is not find in the map then add in map as well 
+            //as queue
+            if(mpp.find(s[i]) == mpp.end()){
+                q.push(i);
             }
-            if(count == 0) return i;
+             mpp[s[i]]++;    //in map increasing the frequency
+             
+            //now we have to remove repeating character from queue
+            while(q.size()>0 &&  mpp[s[q.front()]]>1){
+                q.pop();
             }
-        return -1;   
+        }
+        return q.empty() ? -1: q.front();
+
         }
         
 };
