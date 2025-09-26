09ba95cf1b955755f54110f7ac1acdc62816cf96
Kanishka Agrawal <112953546+kanishkaag@users.noreply.github.com>
2025-09-26 23:04:11 +0530

    Time: 3 ms (69.85%), Space: 15 MB (20%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,14 +3,13 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int> mpp;
         int complement = 0;
-        for(int i = 0;i<nums.size();i++){
+        for(int i = 0; i < nums.size(); i++){
             complement = target - nums[i];
-            if(mpp.find(complement)!=mpp.end()){
-                return {mpp[complement], i};
+            if(mpp.find(complement) != mpp.end()){
+                return {i,mpp[complement]};
             }
             mpp[nums[i]] = i;
         }
         return {};
-        
     }
 };
