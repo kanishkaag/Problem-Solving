896f6b0cf59d4229e1e4e2a59e4c6713b9636d75
Kanishka Agrawal <112953546+kanishkaag@users.noreply.github.com>
2025-10-04 10:42:29 +0530

    Time: 0 ms (100%), Space: 23.8 MB (55.44%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i = 0;  //variable for keeping non zero element
-        for(int j = 0;j< nums.size();j++){
+        int i = 0;
+        for(int j = 0;j<nums.size();j++){
             if(nums[j] != 0){
-                swap(nums[j],nums[i]);
-                i++;
+                //if(i!=j){ // just to prevent extra swapping at same place when i and j
+                    swap(nums[j] , nums[i]);
+                    i++;
+                //}  
             }
         }
     }
-}; 
+};
