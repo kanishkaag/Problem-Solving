62a4ab5857cefd56d40d45f6173bb51a429afb62
Kanishka Agrawal <112953546+kanishkaag@users.noreply.github.com>
2025-11-05 01:18:07 +0530

    Time: 0 ms (100%), Space: 12.9 MB (14.77%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans;
-        for(int i = 0; i < nums1.size(); i++){
-            int val = nums1[i];
-            int nextGreater = -1;
-            for(int j = 0; j < nums2.size(); j++){
-                if(nums2[j] == val){
-                    for (int k = j + 1; k < nums2.size(); k++) {
-                        if (nums2[k] > val) {
-                            nextGreater = nums2[k];
-                            break;
-                        }
-                    }
-                    break; // Stop searching once found in nums2
-                }
+        //optimized
+        unordered_map<int, int> m; //nums2[i], NG
+        stack<int> st;
+        for(int i = nums2.size()-1; i>=0; i--){
+            while(st.size() > 0 && st.top() <= nums2[i]){
+                st.pop();
+            }
+            if(st.empty()){
+                m[nums2[i]] = -1;
+            }
+            else{
+                m[nums2[i]] = st.top();
             }
 
-            ans.push_back(nextGreater);
+            st.push(nums2[i]);
+        }
+        vector<int> ans;
+        for(int i = 0; i < nums1.size(); i++){
+            ans.push_back(m[nums1[i]]);
         }
         return ans;
-
-                    // if(j + 1 < nums2.size() && nums2[j] < nums2[j + 1]){
-                    //     ans.push_back(nums2[j+1]);
-                    // }
-                    // else{
-                    //     ans.push_back(-1);   
     }
 };
