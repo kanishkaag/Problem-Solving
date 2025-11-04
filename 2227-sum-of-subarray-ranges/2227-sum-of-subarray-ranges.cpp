13e7cca4e63f8caa3a7dd0e8a547755c41691433
Kanishka Agrawal <112953546+kanishkaag@users.noreply.github.com>
2025-11-04 22:42:52 +0530

    Time: 18 ms (16.66%), Space: 14.1 MB (79.03%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
+        //BRUTE FORCE
         long long ans = 0;
         int maxE = INT_MIN;
         int minE = INT_MAX;
         for (int i = 0; i < nums.size(); i++) {
             int maxi = nums[i];
             int mini = nums[i];
-            for (int j = i; j < nums.size(); j++) {
+            for (int j = i+1; j < nums.size(); j++) {
                 maxi = max(maxi, nums[j]);
                 mini = min(mini, nums[j]);
                 ans += maxi - mini;
