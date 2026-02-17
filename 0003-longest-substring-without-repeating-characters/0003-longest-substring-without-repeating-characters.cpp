1b3634780fc5d494796bca14e25c311adec87b06
Kanishka Agrawal <112953546+kanishkaag@users.noreply.github.com>
2026-02-17 23:42:34 +0530

    Time: 24 ms (13.9%), Space: 14.4 MB (21.06%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,24 +4,18 @@ public:
         int i = 0, j = 0;
         int len = 0;
         unordered_set<char> st;
-      
         while(j < s.size()){
             if(st.find(s[j]) == st.end()){
-                st.insert(s[j]);
-                
+                st.insert(s[j]); 
                 len = max(len , j-i+1);
                 j++;
             }
             else{
                 st.erase(s[i]);
-                i++;
-                
+                i++; 
                 //len = max(len , j-i+1);
             }
-         
-
         }
-        return len;
-        
+        return len; 
     }
 };
